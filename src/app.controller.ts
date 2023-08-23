3dd6e27a9b41f728c130033544a269c09359e64d
hebertcisco <hebertcisco@outlook.com>
2023-08-22 23:40:07 -0300

    update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(@Query() message: string) {
-    return this.appService.getHello(message);
+  getHello(@Query() query: { message?: string }) {
+    return this.appService.getHello(query?.message);
   }
 }
